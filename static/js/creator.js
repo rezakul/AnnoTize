06cb7e418557cd3fa7d8fb17e73351ac72aecd7e
Lukas Panzer <lukas.panzer@fau.de>
2023-11-16 18:12:33 +0100

    add comments

--- static/js/creator.js
+++ static/js/creator.js
@@ -1,14 +1,21 @@
+/**
+ * Holds information about the creator of the annotations.
+ */
 class Creator {
-  #id;
-  #annotationStyle = 'default';
-  #annotationStyleOverwritable = true;
+  #id;                              // unique creator id
+  #annotationStyle;                 // the annotation highlight style new annotations of this creator have
+  #annotationStyleOverwritable;     // flag if style is overwritable
 
   /**
    * Annotation Creator
    * @param {string} id the unique creator id
+   * @param {string} style the highlight style ('default' if omitted)
+   * @param {boolean} overwritable  flag if style is overwritable (true if omitted)
    */
-  constructor (id) {
+  constructor (id, style='default', overwritable=true) {
     this.#id = id;
+    this.#annotationStyle = style;
+    this.#annotationStyleOverwritable = overwritable;
   }
 
   /**
